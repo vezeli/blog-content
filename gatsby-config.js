9221b6f3f463faa40194f1b0fd1c16820a8157bf
Velibor Zeli <velibor@mech.kth.se>
2020-05-22 10:17:31 +0200

    Fix RSS Feeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,17 @@ module.exports = {
   siteMetadata: {
     siteTitle: `@myletters`,
     siteTitleAlt: `Velibor Želi - Busy with PhD studies & software developing`,
+    siteUrl: `https://mylette.rs`,
+    siteDescription: `Velibor Želi's blog`,
     author: `Velibor Želi`,
+    siteImage: ``,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
+	feedTitle: "Velibor Želi's blog",
         navigation: [
           {
             title: `Blog`,
@@ -39,12 +43,12 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
+    //{
+    //  resolve: `gatsby-plugin-google-analytics`,
+    //  options: {
+    //    trackingId: process.env.GOOGLE_ANALYTICS_ID,
+    //  },
+    //},
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -52,13 +56,13 @@ module.exports = {
         name: `@myletters`,
         short_name: `@myletters`,
         description: `Personal blog.`,
-        start_url: `/`,
+        start_url: `https://mylette.rs`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
+    //`gatsby-plugin-offline`,
+    //`gatsby-plugin-netlify`,
   ],
 }
